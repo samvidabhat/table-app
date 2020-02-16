cd164c2e238ce32b8f815e7687f8fbf80af61683
samvida bhat <samvidabhat@gmail.com>
2020-02-16 15:18:04 +0530

    first commit for table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      data:[]
+    }
+  }
+  componentDidMount(){
+    axios.get("https://jsonplaceholder.typicode.com/photos").then(res=>{ 
+      this.setState({data:res.data});
+    })
+  }
+  getHead(){
+    if(this.state.data.length){
+      return <tr key={0}>
+      <th>
+        <input type="checkbox" />
+        </th>
+        <th>
+            Sl.No
+        </th >
+        <th>
+           Title
+        </th>
+        <th>
+            URL
+        </th>
+        </tr>
+    }else{
+     return <div style={{align:'center'}}>No records.</div>
+    }
+    
+  }
+  getBody(){
+    let bodyData=this.state.data && this.state.data.map((ele,i)=>{
+      return <tr key={i+1} width="20px">
+      <td>
+      <input type="checkbox" />
+      </td>
+        <td>
+        {ele.id}
+        </td>
+        <td>
+        <img src={ele.thumbnailUrl} style={{width:"20px"}} />
+        </td>
+        <td>
+        {ele.title}
+        </td>
+      </tr>
+    });
+    return bodyData;
+    
+  }
+  render(){
+    return <div align='center'>
+        <table>
+          <thead>
+            {this.getHead()}
+            </thead>
+            
+            <tbody>
+              {this.getBody()}
+              </tbody>
+          </table>
     </div>
-  );
+  }
 }
-
-export default App;
